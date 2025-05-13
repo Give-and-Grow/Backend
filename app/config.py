cd6aa3893b5e618c2708fd7bd953b568d6efc89e
Bara'a Shelbaya <133972448+Bar-aa@users.noreply.github.com>
2025-05-13 22:44:03 +0300

    posts system without reactions

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
-#Backend/app/config.py
+# Backend/app/config.py
 import os
 from datetime import timedelta
-
+import firebase_admin
+from firebase_admin import credentials, firestore, messaging
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,28 +11,28 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = (
-    f"mysql+pymysql://{os.getenv('MYSQL_DATABASE_USER')}:"
-    f"{os.getenv('MYSQL_DATABASE_PASSWORD')}@"
-    f"{os.getenv('MYSQL_DATABASE_HOST')}/"
-    f"{os.getenv('MYSQL_DATABASE_DB')}"
+        f"mysql+pymysql://{os.getenv('MYSQL_DATABASE_USER')}:" +
+        f"{os.getenv('MYSQL_DATABASE_PASSWORD')}@" +
+        f"{os.getenv('MYSQL_DATABASE_HOST')}/" +
+        f"{os.getenv('MYSQL_DATABASE_DB')}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-
-    # JWT Configuration
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "123")
     JWT_BLACKLIST_ENABLED = True
     JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
 
-    # Mail settings
     MAIL_SERVER = os.getenv("MAIL_SERVER")
     MAIL_PORT = int(os.getenv("MAIL_PORT"))
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS") == "True"
     MAIL_USERNAME = os.getenv("MAIL_USERNAME")
     MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv(
-        "MAIL_DEFAULT_SENDER", "baraa.shellbaya@gmail.com"
-    )
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "baraa.shellbaya@gmail.com")
+
+
+cred = credentials.Certificate(os.getenv("FIREBASE_CREDENTIAL_PATH"))
+firebase_admin.initialize_app(cred)
+
+db_firestore = firestore.client()
