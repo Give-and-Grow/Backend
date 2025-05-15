ad4306aee3b4f2df8c217038bcb259b20c1d8724
Bara'a Shelbaya <133972448+Bar-aa@users.noreply.github.com>
2025-05-15 12:31:01 +0300

    dash

--- app/models/account.py
+++ app/models/account.py
@@ -17,6 +17,7 @@ class Role(enum.Enum):
 class Account(db.Model):
     __tablename__ = "account"
 
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
     password = db.Column(db.String(128), nullable=False)
